a7831db08edf78ec6343d30a22cbad0be16d4001
root <tarik.audi@hotmail.com>
2022-02-04 07:43:20 -0800

    alphabet2

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+/**
+ * main - print all the alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+	char d;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	putchar(c);
+	}
+
+	for (d = 'A'; d <= 'Z'; d++)
+	{
+	putchar(d);
+	}
+	putchar('\n');
+	return (0);
+}
