7ba00d48b71af2ee90b31542db848dd831f3585d
root <tarik.audi@hotmail.com>
2022-02-04 07:06:21 -0800

    alphabet

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,6 +1,5 @@
-#include<stdlib.h>
-#include<time.h>
-#include<stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
  * main - compare a random number if it is negative positive or 0
@@ -10,14 +9,11 @@
 int main(void)
 {
 	int n;
+	int LastDigit = n % 10;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n == 0)
-	printf("%d is zero\n", n);
-	else if (n < 0)
-	printf("%d is negative\n", n);
-	else if (n > 0)
-	printf("%d is positive\n", n);
+	
+	if (
 	return (0);
 }
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main - print all the alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+		
+	}
+	putchar ('\n');
+	return (0);
+}	
