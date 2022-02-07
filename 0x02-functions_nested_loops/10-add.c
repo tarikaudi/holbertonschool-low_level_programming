394517fe3e6ce53b8bb105e5c67874d7b00d861f
root <tarik.audi@hotmail.com>
2022-02-07 10:04:35 -0800

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *add - adds two integers
+ *@i: variable stores another
+ *@b: variable stores one
+ *Return: return return i + b
+*/
+int add(int i, int b)
+{
+	int c;
+
+	c = (i + b);
+	return (c);
+
+}
