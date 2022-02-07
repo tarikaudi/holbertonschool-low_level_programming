8d801ee4db1566515b9f6a3b1dea61a1f50d8d61
root <tarik.audi@hotmail.com>
2022-02-07 11:57:29 -0800

    hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main"
+
+/**
+ *jack_bauer - prints hours and minuts
+ *
+ *Return: 0
+*/
+void jack_bauer(void)
+{
+	int h, m;
+
+	while (h <= 24)
+	{
+		while (m <= 60)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+		}
+	}
+	return (0);
+}
