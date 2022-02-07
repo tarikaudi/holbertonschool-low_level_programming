e4b6038143cd1cb2e067e87c23618d4e0bb01c7b
root <tarik.audi@hotmail.com>
2022-02-07 09:11:26 -0800

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *_abs - show absolute value
+ *@i: variable that is used
+ *Return: return absolute value
+*/
+int _abs(int i)
+{
+	if (i >= 0)
+		return (i);
+	else
+		return (-i);
+}
