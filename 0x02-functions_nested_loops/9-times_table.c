427c409238195ff326c4420e4f209ce6432425dc
root <tarik.audi@hotmail.com>
2022-02-07 17:01:38 -0800

    timesTable

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ *times_table - Show Times table9
+ *
+ *Return: return absolute value
+*/
+void times_table(void)
+{
+	int num, mult, prod;
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar('0');
+
+		for (mult = 1; mult <= 9; mult++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			prod = num * mult;
+
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar('0' + (prod / 10));
+			_putchar('0' + (prod % 10));
+		}
+		_putchar('\n');
+	}
+}
+
