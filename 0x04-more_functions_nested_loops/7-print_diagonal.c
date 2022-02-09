cc051de4d39523d9495848c0e8a0a27d5edcd292
root <tarik.audi@hotmail.com>
2022-02-09 08:50:36 -0800

    diagonals

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,3 +6,17 @@
  *Return: return absolute value
 */
 void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	if (n >= 1)
+	{
+		for (i = 0; i > n; i++)
+		{
+			for (j = 0; j > n; j++)
+				_putchar(' ');
+			_putchar('\\');
+		}
+	}
+}
