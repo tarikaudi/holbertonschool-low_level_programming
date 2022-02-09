3daa8725b1b923b0cbbeca3a5a808305ca3bd724
root <tarik.audi@hotmail.com>
2022-02-09 07:39:35 -0800

    moreNumbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,15 +8,15 @@ void more_numbers(void)
 	int i;
 	int num;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		for (num = 0; num <= 14; num++)
 		{
-			if(num > 9)
+			if (num > 9)
 			{
-				_putchar((num / 10)+ '0');
+				_putchar((num / 10) + '0');
 			}
-				_putchar((num % 10)+ '0');
+				_putchar((num % 10) + '0');
 		}
 	_putchar('\n');
 	}
---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,8 @@
+#include "main.h"
+
+/**
+ *_abs - show absolute value
+ *@i: variable that is used
+ *Return: return absolute value
+*/
+void print_diagonal(int n)
