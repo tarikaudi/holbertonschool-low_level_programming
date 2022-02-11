4065a1632770ca4b2d50d19b999b58f7b10a1438
root <tarik.audi@hotmail.com>
2022-02-11 08:50:46 -0800

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *swap_int - show absolute value
+ *@*a: pointer
+ *@*b: pointer2
+ *Return: return absolute value
+*/
+void swap_int(int *a, int *b)
+{
+	int i;
+
+	i = *a;
+	a* = *b;
+	b* = i;
+}
