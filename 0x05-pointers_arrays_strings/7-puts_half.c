551c722a2bfa43b3274425015f01cb59d5fbac0c
root <tarik.audi@hotmail.com>
2022-02-11 14:26:09 -0800

    putsHalf

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ *puts_half - prits one character and jumps
+ *@str: string should
+ *Return: return absolute value
+*/
+void puts_half(char *str)
+{
+	int i = 0;
+	int j = 0;
+	int d = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+		if (i % 2 == 0)
+			d = i / 2;
+		else
+			d = (i + 1) / 2;
+
+		for (j = n; j < i; j++)
+		{
+			_putchar(str[j]);
+		}
+	_putchar('\n');
+}
