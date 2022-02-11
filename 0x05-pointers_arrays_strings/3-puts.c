470fb0ae074788ada1c74c52cda095f8a0fa14fa
root <tarik.audi@hotmail.com>
2022-02-11 09:22:55 -0800

    PrintString

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *_puts - print the string
+ *@str: pointer of a string
+ *Return: return absolute value
+*/
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+	}
+}
+
