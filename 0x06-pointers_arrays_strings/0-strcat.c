27e88e76fea5c58fc1ecb5ee0bbc662b0d7fd982
root <tarik.audi@hotmail.com>
2022-02-15 06:07:57 -0800

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *_strcat - show absolute value
+ *@dest: destiny array with char
+ *@src: array that will be copied
+ *Return: return copy
+*/
+char *_strcat(char *dest, char *src)
+{
+	int i = 0; 
+	int j; 
+
+	while (dest[i] != '\0')
+		i++;
+
+	for (j = 0; src[j] != '\0'; j++, i++) 
+	{
+   		dest[i] = src[j];
+ 	}
+
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *_strlen - show absolute value
+ *@s: pointer
+ *Return: return lenght
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i; 
+	int j;
+
+	for (
+}
