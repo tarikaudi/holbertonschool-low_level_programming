9aeb8e5e513f96a483de81ba0e5479b7c01abf40
root <tarik.audi@hotmail.com>
2022-02-16 04:08:59 -0800

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,16 +8,16 @@
 */
 char *_strcat(char *dest, char *src)
 {
-	int i = 0; 
-	int j; 
+	int i = 0;
+	int j;
 
 	while (dest[i] != '\0')
 		i++;
 
-	for (j = 0; src[j] != '\0'; j++, i++) 
-	{
-   		dest[i] = src[j];
- 	}
+		for (j = 0; src[j] != '\0'; j++, i++)
+		{
+		dest[i] = src[j];
+		}
 
 	return (dest);
 }
