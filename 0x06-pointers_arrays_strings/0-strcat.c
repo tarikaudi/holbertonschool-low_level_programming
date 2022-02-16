6542ed7a30b106f96f3e4b1e4ec2ac184bfef2c0
root <tarik.audi@hotmail.com>
2022-02-16 04:16:47 -0800

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,23 +1,23 @@
 #include "main.h"
-
 /**
- *_strcat - show absolute value
- *@dest: destiny array with char
- *@src: array that will be copied
- *Return: return copy
-*/
+ *_strcat - concatenate two strings
+ *@dest: string
+ *@src: string
+ *Return: dest
+ **/
+
 char *_strcat(char *dest, char *src)
 {
 	int i = 0;
-	int j;
+	int n;
 
 	while (dest[i] != '\0')
+	{
 		i++;
-
-		for (j = 0; src[j] != '\0'; j++, i++)
-		{
-		dest[i] = src[j];
-		}
-
+	}
+	for (n = 0; src[n] != '\0'; n++, i++)
+	{
+		dest[i] = src[n];
+	}
 	return (dest);
 }
