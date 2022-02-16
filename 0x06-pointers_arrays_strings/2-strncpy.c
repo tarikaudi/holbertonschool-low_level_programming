5e81226682b5d863cda1faea5f3282054adc8d1c
root <tarik.audi@hotmail.com>
2022-02-16 05:41:45 -0800

    strcpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,14 +1,23 @@
 #include "main.h"
 
 /**
- *_strlen - show absolute value
- *@s: pointer
- *Return: return lenght
+**_strncpy - function tha concatenate two strings
+* @dest : place where append de src string
+* @src: place of the string for append
+* @n: n is number until print src
+* Return: pointer to dest
 */
+
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i; 
-	int j;
+	int i;
 
-	for (
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	for (; i < n; i++)
+		dest[i] = '\0';
+	return (dest);
 }
