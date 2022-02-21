973789b188dc4f46e78f5c5bf2740ec64c401808
root <tarik.audi@hotmail.com>
2022-02-21 08:32:48 -0800

    strbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,2 +1,26 @@
 #include "main.h"
 
+/**
+ * _strpbrk - prints depending of accept
+ * @s: first string
+ * @accept: second string
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i = 0;
+	int j;
+
+	while (s[i] != '\0')
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (*s == accept[j])
+			{
+				return (s)
+			}
+		}
+	i++;
+	s++;
+	}
+	return ('\0');
+}
