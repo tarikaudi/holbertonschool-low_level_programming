97c4d6fe2a3545f8e07b95cea23402653e1d2634
root <tarik.audi@hotmail.com>
2022-02-25 08:28:54 -0800

    mulargc

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,8 +3,8 @@
 /**
  *main - main main
  *@argc: arguments
- *@argv: strings of pointers 
- *Return: 0 for 
+ *@argv: strings of pointers
+ *Return: 0 for
  **/
 int main(int argc, char __attribute__((unused)) *argv[])
 {
