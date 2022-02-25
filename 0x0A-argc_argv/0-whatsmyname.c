af2320419a1bb2437ee16158edf51b044bf693b0
root <tarik.audi@hotmail.com>
2022-02-25 05:30:54 -0800

    ARGV

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *main - main main
+ *@argv: string of pointers
+ *@argc: pointer
+ *Return: 0 otherwise
+ **/
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *main - main main
+ *@argc: arguments
+ *@argv: strings of pointers 
+ *Return: 0 for 
+ **/
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
