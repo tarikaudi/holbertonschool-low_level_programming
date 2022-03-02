d33d460358fb6f3c5a06adbb50b8c0b1f040a005
root <tarik.audi@hotmail.com>
2022-03-02 06:06:33 -0800

    createArray

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,27 +2,32 @@
 #include <stdlib.h>
 
 /**
- *main - main main
- *@argc: arguments
- *@argv: strings of pointers
+ *create_array - main main
+ *@size: size size
+ *@c: char
  *Return: 0 for
  **/
 char *create_array(unsigned int size, char c)
 {
 	char *arre;
-	unsigned int i = 0;	
-	
+	unsigned int i = 0;
+
 	if (size == 0)
 	{
 		return (NULL);
 	}
-	
+
 	arre = malloc(sizeof(char) * size);
+
+	if (arre == NULL)
+	{
+		return (NULL);
+	}
 
 	for (i = 0; i < size; i++)
 	{
 		arre[i] = c;
 	}
-	
+
 	return (arre);
 }
