ad1d8527e2a1a3b9b4dc790e4b63b5aae0f8d1ca
root <tarik.audi@hotmail.com>
2022-03-03 17:23:35 -0800

    mallocCheck

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+**malloc_checked - main main
+*@b: vari
+*Return: 0 as always
+*/
+
+void *malloc_checked(unsigned int b)
+{
+	void *arre;
+
+	ar = malloc(b);
+	if (ar == NULL)
+	{
+		exit(98);
+	}
+
+	return (ar);
+}
