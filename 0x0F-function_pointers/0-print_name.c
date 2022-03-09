5185fe159bef38335dfa72dd6595c32fe9907065
root <tarik.audi@hotmail.com>
2022-03-09 08:51:28 -0800

    PrintName

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ *print_name - main main
+ *@name: name
+ *@f: pointer
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL || f != NULL)
+		f(name);
+}
