6c797a31458e30d966ced06ede6098d8635a0c43
root <tarik.audi@hotmail.com>
2022-03-17 11:50:04 -0700

    FreeTry

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -9,11 +9,12 @@ void free_list(list_t *head)
 {
 	list_t *list = *head;
 	list_t *list2 = NULL;
-	
+
 	while (list != NULL)
 	{
 		list2 = list->list2;
 		free(list);
 		list = list2;
 	}
+	*head = NULL;
 }
