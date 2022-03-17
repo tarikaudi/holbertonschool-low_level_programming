d2300be1d745d2fdb5e6e59a02857077d16c94fb
root <tarik.audi@hotmail.com>
2022-03-17 11:47:20 -0700

    Free

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ *free_list - main main
+ *@head: pointer to list
+ *Return: return
+ **/
+void free_list(list_t *head)
+{
+	list_t *list = *head;
+	list_t *list2 = NULL;
+	
+	while (list != NULL)
+	{
+		list2 = list->list2;
+		free(list);
+		list = list2;
+	}
+}
