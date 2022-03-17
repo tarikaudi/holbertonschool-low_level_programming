99e599ed5e024031b1af9fe98aee0f311b41ac7f
root <tarik.audi@hotmail.com>
2022-03-17 11:23:23 -0700

    100void

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+ *main_void - main main
+ **/
+void main_void (void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
