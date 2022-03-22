347cdf52b7eef3f2596eccdd8c44e7d309d3fac3
root <tarik.audi@hotmail.com>
2022-03-22 11:16:13 -0700

    Free

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ *free_list - main main
+ *@head: pointer
+ *Return: as always
+ **/
+void free_list(list_t *head)
+{
+	list_t *actual, *next;
+
+	actual = head;
+
+	while (actual != NULL)
+	{
+		next = actual->next;
+		free(actual->str);
+		free(actual);
+		actual = next;
+	}
+}
