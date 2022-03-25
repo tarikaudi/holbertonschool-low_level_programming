9ddee8002f0c73908a85eb1e90a67be3c755adf4
root <tarik.audi@hotmail.com>
2022-03-24 19:09:55 -0700

    getbit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *get_bit - mai main
+ *@n: int
+ *@index: index
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index > 63)
+		return (-1);
+	if (((n >> index) & 1) == 0)
+		return (0);
+	return (1);
+}
