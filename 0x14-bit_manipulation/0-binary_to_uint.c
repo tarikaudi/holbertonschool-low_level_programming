adc1ce915bb93ab533fe0a78867b0ed7c90e4cb0
root <tarik.audi@hotmail.com>
2022-03-24 19:20:59 -0700

    binary

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -16,8 +16,7 @@ unsigned int binary_to_uint(const char *b)
 	lenght = strlen(b);
 
 	for (i = (lenght - 1); i >= 0; i--)
-	{
-	
+	{	
 		if (b[i] != 48 && b[i] != 49)
 			return (0);
 		if (b[i] == '1')
