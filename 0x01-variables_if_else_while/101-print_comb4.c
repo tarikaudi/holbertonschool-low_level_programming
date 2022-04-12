2d117fa951e22402f418d2ec8add5a641c65cd14
tarikaudi <tarik.audi@hotmail.com>
2022-04-12 10:31:37 -0700

    printcomb

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ *main - print more combinations
+ *Return: 0
+ */
+int main(void)
+{
+	int i, j, f;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = (i + 1); j <= 9; j++)
+		{
+			for (f = (j + 1); f <= 9; f++)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
+					if (i != 7 || j != 8 || f != 9)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
