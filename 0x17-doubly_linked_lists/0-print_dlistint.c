fa5f2b20b666ad9ad8ff643b9b0c69a70a6b52b6
tarikaudi <tarik.audi@hotmail.com>
2022-05-05 08:42:24 -0700

    print

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ *print_dlistint - main main
+ *@h: pointer
+ *Return: counter
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+
+	return (i);
+}
