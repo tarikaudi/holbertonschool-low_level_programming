550ac9cabda8445a0c50281d400d8d6cf1c1d8ff
tarikaudi <tarik.audi@hotmail.com>
2022-05-05 09:40:03 -0700

    add

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -14,6 +14,9 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	if (new_node == NULL)
 		return (NULL);
+	new_node->n = n;
+	new_node->next = NULL;
+	new_node->prev = NULL;
 	if (*head == NULL)
 	{
 		*head = new_node;
@@ -23,10 +26,12 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	{
 		(*head)->prev = new_node;
 	}
-	new_node->n = n;
-	new_node->next = *head;
-
-	(*head) = new_node;
+	if (new_node != NULL)
+	{
+		new_node->n = n;
+		new_node->next = *head;
+		(*head) = new_node;
+	}
 
 	return (new_node);
 }
