2560e9796da83ded3c1b4fc573ae5d06c140323f
tarikaudi <tarik.audi@hotmail.com>
2022-05-05 09:06:22 -0700

    list

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ *print_dlistint - main main
+ *@h: pointer
+ *Return: counter
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);	
+}
