5da357d721d217dcb2b4d65bee8c0ec78b329c3c
tarikaudi <tarik.audi@hotmail.com>
2022-05-27 12:02:35 -0700

    hashprint

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+*hash_table_print - hash create
+*@ht: size
+*/
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int size, idx, flag = 0;
+	hash_node_t *temp;
+
+	if (!ht)
+		return;
+	printf("{");
+	size = ht->size;
+	for (idex = 0; idx < size; idex++)
+	{
+		temp = ht->array[idex];
+		while (temp)
+		{
+			if (idex != 0 && flag == 1)
+				printf(", ");
+			flag = 1;
+			printf("'%s': '%s'", temp->key, temp->value);
+			temp = temp->next;
+		}
+	}
+	printf("}\n");
+}
